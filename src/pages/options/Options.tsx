f4e073e3e000563b4b255b3c97939d84ad3fc4f5
Murilo Pacheco <murilo.pacheco99@gmail.com>
2022-10-22 20:28:24 -0300

    subindo heroku

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import { Cards } from "../../components/Cards/Cards"
 import { CardsSpace, Container, TitlePage } from "./styles";
 
-// const Fade = require('react-reveal/Fade')
-
 export const Options = () => {
 
   return (
